9db520df361e4c1c70c0d800a7f7ecb460fdb9ef
wilkin <wilkin4@gmail.com>
2019-08-23 09:11:40 -0700

    add redux debug

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
@@ -7,9 +7,11 @@ import rootReducer from './reducers'
 
 const store = createStore(rootReducer)
 
+
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    (<Provider store={store}>
+        <App />
+    </Provider>),
+    document.getElementById('root')
 )
+
